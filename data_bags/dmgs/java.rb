c34e37af7a53172cb65bc6cb12c856b32b10dc2b
Patrick Connolly <patrick.c.connolly@gmail.com>
2012-10-04 11:56:21 -0300

    Converted a few more json dmgs.

--- data_bags/dmgs/java.rb
+++
@@ -1,8 +0,0 @@
-dmg_package "JavaForOSX" do
-  source "http://support.apple.com/downloads/DL1572/en_US/"
-  volumes_dir "Java for OS X 2012-005"
-  action :install
-  type "pkg"
-  package_id "com.apple.pkg.JavaForMacOSX107"
-  checksum "e0750c72972b8a2ccbcb3144bb31d74419276387"
-end
--- data_bags/dmgs/libreoffice.rb
+++
@@ -1,6 +0,0 @@
-dmg_package "LibreOffice" do
-  volumes_dir "LibreOffice"
-  source "http://download.documentfoundation.org/libreoffice/stable/3.6.1/mac/x86/LibO_3.6.1_MacOS_x86_install_en-US.dmg"
-  checksum "2c60e1e1f9ae1e84230fb541e961272c4c75f0826f8d590e9bfb8ba2a859f505"
-  action :install
-end
