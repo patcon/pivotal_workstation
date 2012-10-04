4134e2a138a11892dd1def7cce12f53d942f943c
Patrick Connolly <patrick.c.connolly@gmail.com>
2012-10-03 23:32:18 -0300

    Moved to using dmg_package in dmg cookbook.

--- cookbooks-override/pivotal_workstation/metadata.rb
+++ cookbooks-override/pivotal_workstation/metadata.rb
@@ -4,4 +4,6 @@ license           "MIT"
 description       "Configure frequently-used tools for an OSX workstation"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.0.0"
+depends           "bash"
+depends           "dmg"
 supports          "mac_os_x"
