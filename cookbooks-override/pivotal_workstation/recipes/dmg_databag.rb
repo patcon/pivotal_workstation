c8d72792492117dcec295ddd13833147b08e4078
Patrick Connolly <patrick.c.connolly@gmail.com>
2012-10-04 02:31:37 -0300

    Moved chrome and dropbox to json data_bag. Made dmg_databag recipe more flexible.

--- cookbooks-override/pivotal_workstation/recipes/dmg_databag.rb
+++ cookbooks-override/pivotal_workstation/recipes/dmg_databag.rb
@@ -4,10 +4,10 @@ installed_dmgs = node['install_dmgs']
 
 installed_dmgs.each do |dmg|
   d = data_bag_item('dmgs', dmg)
-  dmg_package d['name'] do
-    volumes_dir d['volumes_dir']
-    source d['source']
-    checksum d['checksum']
+  dmg_package d['id'] do
+    d.each do |res_attr, value|
+      send(res_attr, value) unless res_attr == 'id'
+    end
     action :nothing
   end
 end
